744083a29700737ea9583f8b4ab0a669f890d44a
Mariano <marianomarchionna8@gmail.com>
2021-09-08 16:09:06 +0200

    Creazione array e metodi

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@
 // * Creare il navigatore in basso con i pallini bianchi (blu se corrispondente all'immagine attualmente visualizzata)
 // * Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
 Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            index: 0,
+            photos: [
+                'img/image1.jpg',
+                'img/image2.jpg',
+                'img/image3.jpg',
+                'img/image4.jpg'
+            ]
+        },
+        methods: {
+            previousPhoto() {
+                this.index -= 1;
+            },
+            nextPhoto(){
+                this.index += 1;
+            }
+        }
+    }
+)
