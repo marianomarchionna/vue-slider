e5f1b382e85f75e8e09fd42ba1c5c2806476e38a
Mariano <marianomarchionna8@gmail.com>
2021-09-08 15:40:09 +0200

    Scaffolding

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Descrizione:
+// Rifare l'esercizio dello slider come fatto assieme in classe.
+// Bonus:
+// * Creare il navigatore in basso con i pallini bianchi (blu se corrispondente all'immagine attualmente visualizzata)
+// * Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+Vue.config.devtools = true;
